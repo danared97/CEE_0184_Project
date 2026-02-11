09c0d8ca4b3c4fa8451777540748a92375638385
dredhu01 <dana.redhuis@tufts.edu>
2026-02-11 12:45:19 -0500

    general download process in blackmarble

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from blackmarble import BlackMarble, Product
 # ------------------------------------------------------------------------------
 # 1. Define your region of interest
 # ------------------------------------------------------------------------------
-# In this example ,we load a region from a GeoJSON.
-gdf = gpd.read_file("path/to/your/shapefile.geojson")
+# input shapefile here
+gdf = gpd.read_file("path")
 
 # ------------------------------------------------------------------------------
 # 2. Set up the BlackMarble client
 # ------------------------------------------------------------------------------
 # If the environment variable `BLACKMARBLE_TOKEN` is set, it will be used automatically.
 # You can also pass your token directly, but using the environment variable is recommended.
+#I already have my token and everything set up!
 bm = BlackMarble(token="YOUR_BLACKMARBLE_TOKEN")
 
 # ------------------------------------------------------------------------------
@@ -24,5 +25,5 @@ bm = BlackMarble(token="YOUR_BLACKMARBLE_TOKEN")
 raster_earth_day = bm.raster(
     gdf,
     product_id=Product.VNP46A2,
-    date_range="2025-04-22",
+    date_range="",
 )
