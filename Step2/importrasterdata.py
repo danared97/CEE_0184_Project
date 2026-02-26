dc0d5c8fa8f812d39022b81c67c60fbcf5334948
dredhu01 <dana.redhuis@tufts.edu>
2026-02-26 16:07:59 -0500

    added more comments

--- Step2/importrasterdata.py
+++ Step2/importrasterdata.py
@@ -1,11 +1,24 @@
 #import raster worldpop data as a raster array
-
 #finer-resolution data for the populations considered to be higher concern - south korea over 65, argentina under 20
+
+#we also have finer-resolution california data in a shapefile (California NRI and cal_demographics, saved here: https://drive.google.com/file/d/1_Q1GzXPh6rnh3IQDzpZ7rFto9tBqn_z0/view?usp=drive_link
+#this could probably be incorporated just with geopandas
+#incorporate columns for cal_demographics:
+# - median_house_income
+# - population above 65
+# - populatio below 18
+
+#incorporate columns for cal_NRI:
+# - WFIR_RISKR, 'Wildfire - Hazard Type Risk Index Score' (range from very low to very high)
+# - SOVI_SPCTL, 'Social Vulnerability - State Percentile'
+# - RESL_SPCTL, 'Community Resilience - State Percentile'
+
+#southkorea tifs link: https://drive.google.com/file/d/1nBf3dLpHBulVz1sxVnBVhigz2Fz1ujGp/view?usp=drive_link
+#argentina tifs link: https://drive.google.com/file/d/1zWhd7kH9fyG5Dd-3UHqgNZL0e5sGPvtM/view?usp=drive_link
 
 import rasterio
 import glob
 import numpy as np
-
 
 def process_worldpop_rasters(file_paths):
     data_layers = {}
